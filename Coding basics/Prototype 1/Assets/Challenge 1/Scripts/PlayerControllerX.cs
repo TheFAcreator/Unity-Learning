bf7779ea3faa6ba37c37bca13bee641aeedbac28
TheFAcreator <bor.stanchev@gmail.com>
2025-11-30 21:38:16 +0200

    Add challenge scene

---
+++ Coding basics/Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    private float verticalInput;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime); - not physics based (not recommended for Rigidbody)
+        playerRb.linearVelocity = transform.forward * speed; // physics based (recommended for Rigidbody)
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
